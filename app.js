415deae99025f99b472def91a382f547eee15342
QuliyevaLeyla <quliyevaleyla1412@gmail.com>
2025-04-07 21:12:22 +0400

    I added files

---
+++ app.js
@@ -0,0 +1,10 @@
+let img = document.getElementById("image")
+
+// FUNCTIONS
+function on(){
+    img.src = "./images/pic_bulbon.gif"
+}
+
+function off(){
+    img.src = "./images/pic_bulboff.gif"
+}
